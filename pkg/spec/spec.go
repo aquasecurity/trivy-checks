6f460ada2a299fc7422922523ef23f910fc44baf
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-09-13 13:25:43 +0700

    add spec pkg

---
+++ pkg/spec/spec.go
@@ -0,0 +1,23 @@
+package spec
+
+import (
+	"github.com/simar7/trivy-misconf-rules/rules/specs"
+)
+
+// Loader access compliance specs
+type Loader interface {
+	GetSpecByName(name string) string
+}
+
+type specLoader struct {
+}
+
+// NewSpecLoader instansiate spec loader
+func NewSpecLoader() Loader {
+	return &specLoader{}
+}
+
+// GetSpecByName get spec name and return spec data
+func (sl specLoader) GetSpecByName(name string) string {
+	return specs.GetSpec(name)
+}
