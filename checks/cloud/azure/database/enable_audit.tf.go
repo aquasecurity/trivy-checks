f643f80bda8aef4f6e76757a9ccb46e7aa6f613a
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-03-05 15:37:37 +0700

    fix(azure): update remediation for AVD-AZU-0027

--- checks/cloud/azure/database/enable_audit.tf.go
+++ checks/cloud/azure/database/enable_audit.tf.go
@@ -9,13 +9,14 @@ var terraformEnableAuditGoodExamples = []string{
    version                      = "12.0"
    administrator_login          = "mradministrator"
    administrator_login_password = "tfsecRocks"
- 
-   extended_auditing_policy {
-     storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
-     storage_account_access_key              = azurerm_storage_account.example.primary_access_key
-     storage_account_access_key_is_secondary = true
-     retention_in_days                       = 6
-   }
+ }
+
+ resource "azurerm_mssql_server_extended_auditing_policy" "example" {
+  server_id                               = azurerm_sql_server.good_example.id
+  storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+  storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+  storage_account_access_key_is_secondary = true
+  retention_in_days                       = 6
  }
  `,
 }
@@ -34,7 +35,7 @@ var terraformEnableAuditBadExamples = []string{
 }
 
 var terraformEnableAuditLinks = []string{
-	`https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/sql_server#extended_auditing_policy`,
+	`https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_server_extended_auditing_policy`,
 }
 
 var terraformEnableAuditRemediationMarkdown = ``
