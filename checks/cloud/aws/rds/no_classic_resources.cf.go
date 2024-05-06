5a025adddfa0badf3ebeca3a9d319f3690dbe565
Steven Smiley <smiley@scalesec.com>
2024-05-06 10:30:31 -0700

    Revert change to classic resources examples

--- checks/cloud/aws/rds/no_classic_resources.cf.go
+++ checks/cloud/aws/rds/no_classic_resources.cf.go
@@ -2,11 +2,23 @@ package rds
 
 var cloudFormationNoClassicResourcesGoodExamples = []string{
 	`---
+AWSTemplateFormatVersion: 2010-09-09
+Description: Good example of rds sgr
+Resources:
+
 `,
 }
 
 var cloudFormationNoClassicResourcesBadExamples = []string{
 	`---
+AWSTemplateFormatVersion: 2010-09-09
+Description: Bad example of rds sgr
+Resources:
+  Queue:
+    Type: AWS::RDS::DBSecurityGroup
+    Properties:
+      Description: ""
+
 `,
 }
 
--- checks/cloud/aws/redshift/no_classic_resources.cf.go
+++ checks/cloud/aws/redshift/no_classic_resources.cf.go
@@ -2,11 +2,23 @@ package redshift
 
 var cloudFormationNoClassicResourcesGoodExamples = []string{
 	`---
+AWSTemplateFormatVersion: 2010-09-09
+Description: Good example of redshift sgr
+Resources:
+
 `,
 }
 
 var cloudFormationNoClassicResourcesBadExamples = []string{
 	`---
+AWSTemplateFormatVersion: 2010-09-09
+Description: Bad example of redshift sgr
+Resources:
+  Queue:
+    Type: AWS::Redshift::ClusterSecurityGroup
+    Properties:
+      Description: ""
+
 `,
 }
 
