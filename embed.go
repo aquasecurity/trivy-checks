7f7bdd48d124d2fa3c6a73a8382ef4db7fffc04b
chenk <hen.keinan@gmail.com>
2024-05-30 15:43:03 +0300

    feat: embedded commands fs support
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- embed.go
+++ embed.go
@@ -9,3 +9,9 @@ var EmbeddedPolicyFileSystem embed.FS
 
 //go:embed lib/*
 var EmbeddedLibraryFileSystem embed.FS
+
+//go:embed commands/kubernetes/*
+var EmbeddedK8sCommandsFileSystem embed.FS
+
+//go:embed commands/config/*
+var EmbeddedConfigCommandsFileSystem embed.FS
