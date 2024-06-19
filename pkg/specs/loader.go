f70b803f237b3ab439e712e34146401943df7052
Simar <simar@linux.com>
2024-06-18 23:37:03 -0600

    feat(specs): Expose specs as a pkg

--- pkg/specs/loader.go
+++ pkg/specs/loader.go
@@ -14,20 +14,20 @@ const ComplianceFolder = "compliance"
 
 var (
 	//go:embed compliance
-	complainceFS embed.FS
+	complianceFS embed.FS
 )
 
 var complianceSpecMap map[string]string
 
 // Load compliance specs
 func init() {
-	dir, _ := complainceFS.ReadDir(ComplianceFolder)
+	dir, _ := complianceFS.ReadDir(ComplianceFolder)
 	complianceSpecMap = make(map[string]string, 0)
 	for _, r := range dir {
 		if !strings.Contains(r.Name(), ".yaml") {
 			continue
 		}
-		file, err := complainceFS.Open(fmt.Sprintf("%s/%s", ComplianceFolder, r.Name()))
+		file, err := complianceFS.Open(fmt.Sprintf("%s/%s", ComplianceFolder, r.Name()))
 		if err != nil {
 			panic(err)
 		}
