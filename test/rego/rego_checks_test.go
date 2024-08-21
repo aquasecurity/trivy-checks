570238ebfeb5d75bff38bd74bacbecafc8ec8a41
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-08-21 09:57:27 +0600

    test: add functional tests
    Signed-off-by: Nikita Pivkin <nikita.pivkin@smartforce.io>

--- test/rego/rego_checks_test.go
+++ test/rego/rego_checks_test.go
@@ -62,15 +62,17 @@ func TestRegoChecks(t *testing.T) {
 		azureAuthorizationTestCases,
 		azureContainerTestCases,
 
-    googleDnsTestCases,
+		googleDnsTestCases,
 		googleKmsTestCases,
 		googleBigQueryTestCases,
 
-    githubTestCases,
+		githubTestCases,
 
-    nifcloudDnsTestCases,
+		nifcloudDnsTestCases,
 		nifcloudNetworkTestCases,
 		nifcloudSslCertificateTestCases,
+
+		oracleTestCases,
 	)
 
 	regoScanner := rego.NewScanner(trivyTypes.SourceCloud)
