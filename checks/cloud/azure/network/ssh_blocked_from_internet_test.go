596af4101548129bc059c964d7cecf125e583af0
Nikita Pivkin <nikita.pivkin@smartforce.io>
2024-08-22 10:49:29 +0600

    test: update tests
    Signed-off-by: Nikita Pivkin <nikita.pivkin@smartforce.io>

--- checks/cloud/azure/network/ssh_blocked_from_internet_test.go
+++ checks/cloud/azure/network/ssh_blocked_from_internet_test.go
@@ -33,8 +33,8 @@ func TestCheckSshBlockedFromInternet(t *testing.T) {
 								DestinationPorts: []network.PortRange{
 									{
 										Metadata: trivyTypes.NewTestMetadata(),
-										Start:    22,
-										End:      22,
+										Start:    trivyTypes.IntTest(22),
+										End:      trivyTypes.IntTest(22),
 									},
 								},
 								SourceAddresses: []trivyTypes.StringValue{
@@ -62,8 +62,8 @@ func TestCheckSshBlockedFromInternet(t *testing.T) {
 								DestinationPorts: []network.PortRange{
 									{
 										Metadata: trivyTypes.NewTestMetadata(),
-										Start:    22,
-										End:      22,
+										Start:    trivyTypes.IntTest(22),
+										End:      trivyTypes.IntTest(22),
 									},
 								},
 								SourceAddresses: []trivyTypes.StringValue{
@@ -91,8 +91,8 @@ func TestCheckSshBlockedFromInternet(t *testing.T) {
 								DestinationPorts: []network.PortRange{
 									{
 										Metadata: trivyTypes.NewTestMetadata(),
-										Start:    22,
-										End:      22,
+										Start:    trivyTypes.IntTest(22),
+										End:      trivyTypes.IntTest(22),
 									},
 								},
 								SourceAddresses: []trivyTypes.StringValue{
