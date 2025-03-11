b6d4b523862c4e0ea8980fd979e70ccb5073bde1
simar7 <1254783+simar7@users.noreply.github.com>
2025-03-10 23:58:03 -0600

    refactor(deps): Use OPA v1 (#358)

--- internal/rego/squealer.go
+++ internal/rego/squealer.go
@@ -3,10 +3,10 @@ package rego
 import (
 	"fmt"
 
-	"github.com/open-policy-agent/opa/ast"
-	"github.com/open-policy-agent/opa/rego"
-	"github.com/open-policy-agent/opa/topdown/builtins"
-	"github.com/open-policy-agent/opa/types"
+	"github.com/open-policy-agent/opa/v1/ast"
+	"github.com/open-policy-agent/opa/v1/rego"
+	"github.com/open-policy-agent/opa/v1/topdown/builtins"
+	"github.com/open-policy-agent/opa/v1/types"
 	"github.com/owenrumney/squealer/pkg/squealer"
 )
 
